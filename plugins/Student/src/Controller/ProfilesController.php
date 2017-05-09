1d0f8f18c3be52273955c7dd0887a36046e2acd1
Patrick Ellermann Gorgin <p.gorgin@gmail.com>
2017-05-09 10:33:38 +0200

    "Hotfix"
    To allow Patrick access to the site and other stuff

---
+++ plugins/Student/src/Controller/ProfilesController.php
@@ -0,0 +1,27 @@
+<?php
+namespace Student\Controller;
+
+use Cake\Event\Event;
+
+/**
+ * Dashboard Controller
+ */
+class ProfilesController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->Crud->disable(['index', 'add', 'edit', 'view', 'delete']);
+    }
+
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+    }
+
+    public function index()
+    {
+
+    }
+}
