2498dc62f72077ad92acd9dda76ea64db953ff71
Mikkel Bonde Christensen <mikbonde@gmail.com>
2017-06-06 12:19:37 +0200

    only allow students/assignments/view for now

--- plugins/Student/src/Controller/AssignmentsController.php
+++ plugins/Student/src/Controller/AssignmentsController.php
@@ -23,20 +23,14 @@ class AssignmentsController extends AppController
     {
         parent::beforeFilter($event);
 
-        $this->Crud->on('beforeFind', function (\Cake\Event\Event $event) {
+        $this->Crud->on('beforeFind', function (Event $event) {
             $event->getSubject()->query
-                ->where(['Assignments.organization_id' => $this->Auth->user('active_organization_id')]);
+                ->where([
+                    'Assignments.organization_id' => $this->Auth->user('active_organization_id'),
+                    'Assignments.is_locked' => true,
+                ])
+            ->contain(['CreatedBy']);
         });
-
-        $this->Crud->on('beforePaginate', function (\Cake\Event\Event $event) {
-            $event->getSubject()->query
-                ->where(['Assignments.organization_id' => $this->Auth->user('active_organization_id')]);
-        });
-    }
-
-    public function index()
-    {
-        return $this->Crud->execute();
     }
 
     public function view($id = null)
