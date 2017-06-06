839c6cf8673ea740ceab1e4e1dc719ed4fcc8026
Mikkel Bonde Christensen <mikbonde@gmail.com>
2017-06-06 13:35:03 +0200

    add API plugins for student and teacher separately

---
+++ plugins/StudentApi/tests/TestCase/Controller/AnswerWordFeedbacksControllerTest.php
@@ -0,0 +1,113 @@
+<?php
+namespace StudentApi\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestCase;
+use StudentApi\Controller\AnswerWordFeedbacksController;
+
+/**
+ * StudentApi\Controller\AnswerWordFeedbacksController Test Case
+ */
+class AnswerWordFeedbacksControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.student_api.answer_word_feedbacks',
+        'plugin.student_api.created_by',
+        'plugin.student_api.languages',
+        'plugin.student_api.modified_by',
+        'plugin.student_api.users_roles',
+        'plugin.student_api.users',
+        'plugin.student_api.user_oauth_tokens',
+        'plugin.student_api.courses',
+        'plugin.student_api.courses_users',
+        'plugin.student_api.organizations',
+        'plugin.student_api.countries',
+        'plugin.student_api.currencies',
+        'plugin.student_api.contact_people',
+        'plugin.student_api.roles',
+        'plugin.student_api.answer_words',
+        'plugin.student_api.word_classes',
+        'plugin.student_api.answers',
+        'plugin.student_api.assignments',
+        'plugin.student_api.homeworks',
+        'plugin.student_api.homeworks_courses',
+        'plugin.student_api.homeworks_users',
+        'plugin.student_api.homeworks_assignments',
+        'plugin.student_api.answer_feedbacks'
+    ];
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test beforeFilter method
+     *
+     * @return void
+     */
+    public function testBeforeFilter()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
---
+++ plugins/TeacherApi/tests/TestCase/Controller/AnswerWordFeedbacksControllerTest.php
@@ -0,0 +1,113 @@
+<?php
+namespace TeacherApi\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestCase;
+use TeacherApi\Controller\AnswerWordFeedbacksController;
+
+/**
+ * TeacherApi\Controller\AnswerWordFeedbacksController Test Case
+ */
+class AnswerWordFeedbacksControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.teacher_api.answer_word_feedbacks',
+        'plugin.teacher_api.created_by',
+        'plugin.teacher_api.languages',
+        'plugin.teacher_api.modified_by',
+        'plugin.teacher_api.users_roles',
+        'plugin.teacher_api.users',
+        'plugin.teacher_api.user_oauth_tokens',
+        'plugin.teacher_api.courses',
+        'plugin.teacher_api.courses_users',
+        'plugin.teacher_api.organizations',
+        'plugin.teacher_api.countries',
+        'plugin.teacher_api.currencies',
+        'plugin.teacher_api.contact_people',
+        'plugin.teacher_api.roles',
+        'plugin.teacher_api.answer_words',
+        'plugin.teacher_api.word_classes',
+        'plugin.teacher_api.answers',
+        'plugin.teacher_api.assignments',
+        'plugin.teacher_api.homeworks',
+        'plugin.teacher_api.homeworks_courses',
+        'plugin.teacher_api.homeworks_users',
+        'plugin.teacher_api.homeworks_assignments',
+        'plugin.teacher_api.answer_feedbacks'
+    ];
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test beforeFilter method
+     *
+     * @return void
+     */
+    public function testBeforeFilter()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
